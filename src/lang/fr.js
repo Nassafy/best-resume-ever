40c49ef7758b8e74d76376ccba3306506493e6be
matthias <matthiaspinard@gmail.com>
2018-03-26 00:07:51 +0200

    Mise à jours CV + photo

--- src/lang/fr.js
+++ src/lang/fr.js
@@ -3,8 +3,10 @@ const lang = {
   born: 'Né en',
   bornIn: 'à',
   experience: 'Expérience professionelle',
+  experience_perso: 'Experiences personel',
   education: 'Formation',
   skills: 'Compétences',
-  about: 'À propos de moi'
+  about: 'À propos de moi',
+  loisirs: 'Loisirs / Centres d\'interêts'
 };
 export default lang;
